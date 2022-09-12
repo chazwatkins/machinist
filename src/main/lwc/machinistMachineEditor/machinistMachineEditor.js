38d3d28e8250d0bb7b3504d3b4fe80c15111717d
Chaz Watkins <chazwatkins@hey.com>
2022-09-12 16:53:21 -0500

    add sobjecttype selector

--- src/main/lwc/machinistMachineEditor/machinistMachineEditor.js
+++ src/main/lwc/machinistMachineEditor/machinistMachineEditor.js
@@ -1,10 +1,36 @@
 import {LightningElement, api, track} from 'lwc';
+import newMachine from '@salesforce/apex/Machinist_Controller.newMachine';
+import getSObjectTypeDetails from '@salesforce/apex/Machinist_Controller.getSObjectTypeDetails';
 
 export default class MachinistMachineEditor extends LightningElement {
-    @api machineName;
-    @api sObjectType;
-    @api stateField;
-    @track transitions = [];
+    @api machine = {};
+    @track isRecordTypeSpecific = false;
+    @track sObjectTypeOptions = [];
+    @track sObjectFieldOptions = [];
+    @track recordTypeOptions = [];
 
+    connectedCallback() {
+        newMachine()
+            .then(machineEditor => {
+                this.machine = machineEditor.machine;
+                this.sObjectTypeOptions = machineEditor.sObjectTypeOptions;
+            })
+            .catch(error => {
+
+            })
+    }
+
+    getSObjectTypeDetails() {
+        getSObjectTypeDetails()
+            .then(details => {
+                this.sObjectFieldOptions = details.sObjectFieldOptions;
+                this.recordTypeOptions = details.recordTypeOptions;
+            })
+            .catch(error => {});
+    }
+
+    toggleIsRecordTypeSpecific() {
+        this.isRecordTypeSpecific = !this.isRecordTypeSpecific;
+    }
 
 }
